073d4de0797c7e18ba0e826ebb114801fe512b26
BuildTools <unconfigured@null.spigotmc.org>
2019-04-15 15:00:22 -0500

    added comments

--- zachs_html_parser/_objects.py
+++ zachs_html_parser/_objects.py
@@ -1,9 +1,11 @@
 import re
 
+
 class _href(object):
     def __init__(self, href_string):
         self.text = href_string
 
+    # [6:-1] because the first 6 characters are href=" and the last character is "
     def get_link(self):
         return self.text[6:-1]
 
@@ -19,6 +21,7 @@ class _a(object):
         return self.string()
 
     def href(self):
+        # findall returns a list which should have only one object, so [0]
         raw_href = re.findall(r'href=\s?".+?"', self.text)[0]
         href_class = _href(raw_href)
         return href_class.get_link()
