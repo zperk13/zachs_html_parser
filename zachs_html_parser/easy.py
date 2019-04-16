d6d69d6247dfd5014fe8c6953cfbbd5314102216
BuildTools <unconfigured@null.spigotmc.org>
2019-04-16 15:03:39 -0500

    fixed bug where easy.base_url would mess up on urls with . before

--- zachs_html_parser/easy.py
+++ zachs_html_parser/easy.py
@@ -7,7 +7,6 @@ from zachs_html_parser import tag_finder
 
 def base_url(url):
     base_link_list = re.findall(r'(https://www\.|http://www\.|https://|http://)(.+?(/|$))', url)[0]
-    print(base_link_list)
     base_link = base_link_list[0] + base_link_list[1][:-1]
     return base_link
 
