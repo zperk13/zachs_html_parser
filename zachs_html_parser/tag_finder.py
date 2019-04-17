d9012222151b2ef9aa9693da3dcdfbe1f0269dd2
BuildTools <unconfigured@null.spigotmc.org>
2019-04-16 19:18:06 -0500

    fixed bug called .href on an _a tag with href="" would cause an error fixed a bug where a tags would not be detected if character after > in <a href="" class=...> was a whitespace character

--- zachs_html_parser/tag_finder.py
+++ zachs_html_parser/tag_finder.py
@@ -3,7 +3,9 @@ import re
 from zachs_html_parser import _objects
 
 def a(html):
-    list_of_anchors = []
-    for x in re.findall(r'<a.+?</a>', html):
-        list_of_anchors.append(_objects._a(x))
-    return list_of_anchors
+    pattern = re.compile('(<a.+?</a>|<a\s+?</a>)', re.MULTILINE)
+    matches = pattern.findall(html)
+    real_matches = []
+    for x in matches:
+        real_matches.append(_objects._a(x))
+    return real_matches
