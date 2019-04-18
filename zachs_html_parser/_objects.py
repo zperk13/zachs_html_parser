01aefb8d3106071b19e0237d3a63cc76cbeff346
BuildTools <unconfigured@null.spigotmc.org>
2019-04-18 18:31:02 -0500

    removed print statement from when i was debugging

--- zachs_html_parser/_objects.py
+++ zachs_html_parser/_objects.py
@@ -24,7 +24,6 @@ class _a(object):
         # findall returns a list which should have only one object, so [0]
         try:
             raw_href = re.findall(r"""href=(["'])(.+?)\1""", self.text)[0][1]
-            print(raw_href)
             href_class = _href(raw_href)
             return href_class.get_link()
         except IndexError:
