1c62d059cee19e323c799562368edbf3aaddd1fd
BuildTools <unconfigured@null.spigotmc.org>
2019-04-18 16:19:35 -0500

    if href not found in <a> tag, it will no longer raise an error, and will print what happened

--- zachs_html_parser/_objects.py
+++ zachs_html_parser/_objects.py
@@ -22,9 +22,12 @@ class _a(object):
 
     def href(self):
         # findall returns a list which should have only one object, so [0]
-        raw_href = re.findall(r'href=\s?".*?"', self.text)[0]
-        href_class = _href(raw_href)
-        return href_class.get_link()
+        try:
+            raw_href = re.findall(r'href=\s?".*?"', self.text)[0]
+            href_class = _href(raw_href)
+            return href_class.get_link()
+        except IndexError:
+            print(f'\n\nhref not found for {self.string()}, ignoring\n\n')
 
     def desc(self):
         regex = re.findall(r'>.*?</a>', self.string())[0]
