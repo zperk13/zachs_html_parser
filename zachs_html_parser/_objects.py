218b7086e7dbdf1bccbd9a77fc245ead94366f95
BuildTools <unconfigured@null.spigotmc.org>
2019-04-18 18:52:25 -0500

    fixed bug where href tags with the link starting with https:// or http:// would return //www.whatever instead of https://www.whatever

--- zachs_html_parser/_objects.py
+++ zachs_html_parser/_objects.py
@@ -1,15 +1,6 @@
 import re
 
 
-class _href(object):
-    def __init__(self, href_string):
-        self.text = href_string
-
-    # [6:-1] because the first 6 characters are href=" and the last character is "
-    def get_link(self):
-        return self.text[6:-1]
-
-
 class _a(object):
     def __init__(self, anchor_string):
         self.text = anchor_string
@@ -23,13 +14,12 @@ class _a(object):
     def href(self):
         # findall returns a list which should have only one object, so [0]
         try:
-            raw_href = re.findall(r"""href=(["'])(.+?)\1""", self.text)[0][1]
-            href_class = _href(raw_href)
-            return href_class.get_link()
+            href = re.findall(r"""href=(["'])(.+?)\1""", self.text)[0][1]
+            return href
         except IndexError:
             print(f'\nhref not found for {self.string()}, ignoring\n')
 
     def desc(self):
         regex = re.findall(r'>.*?</a>', self.string())[0]
-        if len(regex)>5:
+        if len(regex) > 5:
             return regex[1:-4]
