7608ccbb62096a5c15d95ab41de4a4c5104268fa
BuildTools <unconfigured@null.spigotmc.org>
2019-04-18 16:23:10 -0500

    fixed bug where weird <a> tags would cause an error

--- zachs_html_parser/easy.py
+++ zachs_html_parser/easy.py
@@ -22,18 +22,19 @@ def all_links(link, html=''):
     links = []
     for x in raw_links:
         # making sure it's actually a link
-        if len(x) > 1:
-            # making sure it's not a link to an id in the page
-            if x[0] != '#':
-                # Checks if it's a redirect to another page in the website
-                if x[0] != '/':
-                    # If not append it to the links list
-                    links.append(x)
-                else:
-                    # If so add the base url in front of it then append it to the links list
-                    base_link = base_url(link)
-                    links.append(base_link + x)
-    return links
+        if x != None:
+            if len(x) > 1:
+                # making sure it's not a link to an id in the page
+                if x[0] != '#':
+                    # Checks if it's a redirect to another page in the website
+                    if x[0] != '/':
+                        # If not append it to the links list
+                        links.append(x)
+                    else:
+                        # If so add the base url in front of it then append it to the links list
+                        base_link = base_url(link)
+                        links.append(base_link + x)
+        return links
 
 
 def safe_all_links(link):
